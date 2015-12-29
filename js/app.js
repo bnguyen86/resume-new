d058f753278c779b04ac70b1bec097d44523a50f
bnguyen86 <bnguyen@outlook.com>
2015-12-29 00:29:42 -0800

    Changed most of page to use new schema and fixed json

--- js/app.js
+++ js/app.js
@@ -1,43 +1,28 @@
 var resumeApp = angular.module('resumeApp',['ui.unique']);
 
   
-resumeApp.controller('contentController', ['$scope', function($scope) {
-  $scope.name = {first:"Ben", last:"Nguyen"};
-}]);
+resumeApp.controller('contentController', ['$scope', '$http', function($scope, $http) {
+  	$scope.name = {first:"Ben", last:"Nguyen"};
+  	$scope.about = '';
+	$scope.skills = [];
+	$scope.projects = [];
+	$scope.education = [];
+	$scope.work = [];
+	$scope.hobbies = [];
 
-resumeApp.controller('skillsController', ['$scope', '$http', function($scope, $http) {
-	$scope.skillsArray = [];
-	$scope.catagories = null;
-	
-	$http.post('/resume/query/', {list: "skill-catagory"}).
+	$http.get('data.json').
 		success(function(data, status, headers, config){
-			$scope.catagories = data;			
-		}).
-		error(function(data, status, headers, config) {
-
-		});
-		
-	$http.post('/resume/query/', {list: "skill-detail"}).
-		success(function(data, status, headers, config){			
-
-		  	for (var i=0; i<data.length; i+=(data.length/3)) {
-				$scope.skillsArray.push(data.slice(i, i+(data.length/3)));
-			}
-			setTimeout(callback, 100);
-			
+			$scope.about = data.about_me;
+			$scope.skills = data.skills;
+			$scope.projects = data.projects;
+			$scope.education = data.education;
+			$scope.work = data.work;
+			$scope.hobbies = data.hobbies;
 		}).
 		error(function(data, status, headers, config) {
 
 		});
 
-  
-}]);
-
-resumeApp.controller('projectsController', ['$scope', '$http', function($scope, $http) {
-	$scope.projects = null;
-	$scope.p_details = null;
-	$scope.p_skills = null;
-	$scope.filterArr = [];
 	$scope.filterBySkill = function(item){
 		var index = $scope.filterArr.indexOf(item);
 			if(index >= 0)
@@ -62,111 +47,121 @@ resumeApp.controller('projectsController', ['$scope', '$http', function($scope, 
 			}
 		}	
 	};
+}]);
+
+
+// resumeApp.controller('projectsController', ['$scope', '$http', function($scope, $http) {
 	
-	$http.post('/resume/query/', {list: "projects-catagory"}).
-		success(function(data, status, headers, config){
-			$scope.projects = data;
-			$http.post('/resume/query/', {list: "projects-skills"}).
-				success(function(data, status, headers, config){
-					$scope.p_skills = data;
-					for(var i = 0; i < $scope.projects.length; i++){
-						var skillsArr = [];
+// 	$scope.p_details = null;
+// 	$scope.p_skills = null;
+// 	$scope.filterArr = [];
 
-						for(var j = 0; j < $scope.p_skills.length; j++)
-							if($scope.projects[i].p_id == $scope.p_skills[j].p_id){
-						 		skillsArr.push($scope.p_skills[j].s_name);
-						 	}
+	
+// 	$http.get('data.json').
+// 		success(function(data, status, headers, config){
+// 			$scope.projects = data;
+// 			$http.get('data.json').
+// 				success(function(data, status, headers, config){
+// 					$scope.p_skills = data;
+// 					for(var i = 0; i < $scope.projects.length; i++){
+// 						var skillsArr = [];
 
-						$scope.projects[i].skills = skillsArr;
-						$scope.projects[i].filtered = true;
-					}
+// 						for(var j = 0; j < $scope.p_skills.length; j++)
+// 							if($scope.projects[i].p_id == $scope.p_skills[j].p_id){
+// 						 		skillsArr.push($scope.p_skills[j].s_name);
+// 						 	}
+
+// 						$scope.projects[i].skills = skillsArr;
+// 						$scope.projects[i].filtered = true;
+// 					}
 
 
-				}).
-				error(function(data, status, headers, config) {
+// 				}).
+// 				error(function(data, status, headers, config) {
 
-				});
-		}).
-		error(function(data, status, headers, config) {
+// 				});
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
+// 		});
 		
-	$http.post('/resume/query/', {list: "projects-detail"}).
-		success(function(data, status, headers, config){
-			$scope.p_details = data;
-		}).
-		error(function(data, status, headers, config) {
+// 	$http.get('data.json').
+// 		success(function(data, status, headers, config){
+// 			$scope.p_details = data;
+// 			callback();
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
+// 		});
 
   
-}]);
+// }]);
 
-resumeApp.controller('workController', ['$scope', '$http', function($scope, $http) {
-	$scope.work = null;
-	$scope.w_details = null;
+// resumeApp.controller('workController', ['$scope', '$http', function($scope, $http) {
+// 	$scope.work = null;
+// 	$scope.w_details = null;
 	
-	$http.post('/resume/query/', {list: "work-catagory"}).
-		success(function(data, status, headers, config){
-			$scope.work = data;
-		}).
-		error(function(data, status, headers, config) {
+// 	$http.get('data.json').
+// 		success(function(data, status, headers, config){
+// 			$scope.work = data;
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
+// 		});
 		
-	$http.post('/resume/query/', {list: "work-detail"}).
-		success(function(data, status, headers, config){
-			$scope.w_details = data;
-		}).
-		error(function(data, status, headers, config) {
+// 	$http.get('data.json', {list: "work-detail"}).
+// 		success(function(data, status, headers, config){
+// 			$scope.w_details = data;
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
-}]);
+// 		});
+// }]);
 
-resumeApp.controller('educationController', ['$scope', '$http', function($scope, $http) {
-	$scope.edu = null;
-	$scope.e_details = null;
+// resumeApp.controller('educationController', ['$scope', '$http', function($scope, $http) {
+// 	$scope.edu = null;
+// 	$scope.e_details = null;
 	
-	$http.post('/resume/query/', {list: "edu-catagory"}).
-		success(function(data, status, headers, config){
-			$scope.edu = data;
-		}).
-		error(function(data, status, headers, config) {
+// 	$http.get('data.json').
+// 		success(function(data, status, headers, config){
+// 			$scope.edu = data;
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
+// 		});
 		
-	$http.post('/resume/query/', {list: "edu-detail"}).
-		success(function(data, status, headers, config){
-			$scope.e_details = data;
-		}).
-		error(function(data, status, headers, config) {
+// 	$http.get('data.json').
+// 		success(function(data, status, headers, config){
+// 			$scope.e_details = data;
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
-}]);
+// 		});
+// }]);
 
-resumeApp.controller('hobbiesController', ['$scope', '$http', function($scope, $http) {
-	$scope.hobbies = null;
+// resumeApp.controller('hobbiesController', ['$scope', '$http', function($scope, $http) {
+// 	$scope.hobbies = [];
 	
-	$http.post('/resume/query/', {list: "hobbies"}).
-		success(function(data, status, headers, config){
-			$scope.hobbies = data;
-		}).
-		error(function(data, status, headers, config) {
+// 	$http.get('data.json').
+// 		success(function(data, status, headers, config){
+// 			$scope.hobbies = data.hobbies;
+// 		}).
+// 		error(function(data, status, headers, config) {
 
-		});
+// 		});
 
-}]);
+// }]);
 
 
-resumeApp.controller('formTester', ['$scope', '$http', function($scope, $http) {
-  $scope.testData = 'TESTING';
-  $scope.submit = function(){
-  	var obj = {data : this.testData};
-  	$http.post('/post/', obj).success(function(data, status, headers, config){
-  		$scope.testData = data;
-  	});
-	 };
-}]);
+// resumeApp.controller('formTester', ['$scope', '$http', function($scope, $http) {
+//   $scope.testData = 'TESTING';
+//   $scope.submit = function(){
+//   	var obj = {data : this.testData};
+//   	$http.get('/post/', obj).success(function(data, status, headers, config){
+//   		$scope.testData = data;
+//   	});
+// 	 };
+// }]);
 
 
 resumeApp.filter('nullToPresent', function() {
